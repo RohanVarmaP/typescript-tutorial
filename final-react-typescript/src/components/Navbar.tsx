fd1c2871fc9dc0fab056f7d36327807ba8777e33
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-10 10:29:55 +0530

    added csss to navbar

--- final-react-typescript/src/components/Navbar.tsx
+++ final-react-typescript/src/components/Navbar.tsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div>
+        <>
             <h1>MY Nav</h1>
-            <Link to='/'><button>Home</button></Link>
-            <Link to='/about'><button>About</button></Link>
-            <Link to='/contact'><button>Contact</button></Link>
-            <Link to='/products'><button>Products</button></Link>
-        </div>
+            <div>
+                <Link to='/'><button className='nav-button'>Home</button></Link>
+                <Link to='/about'><button className='nav-button'>About</button></Link>
+                <Link to='/contact'><button className='nav-button'>Contact</button></Link>
+                <Link to='/products'><button className='nav-button'>Products</button></Link>
+            </div>
+        </>
     )
 }
 
