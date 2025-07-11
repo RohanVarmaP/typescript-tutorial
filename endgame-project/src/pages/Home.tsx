d43e96509a9a95eefdea600c48ff4670707c5380
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-11 11:59:16 +0530

    completed home

--- endgame-project/src/pages/Home.tsx
+++ endgame-project/src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <h1>Home</h1>
+            <p>start game <br /> select difficulty</p>
+            <Link to={'/game?difficulty=easy'}>
+                <button className='home-buttons'>Easy(two clues)</button>
+            </Link>
+            <Link to={'/game?difficulty=standard'}>
+                <button className='home-buttons'>Standard(1 clue)</button>
+            </Link>
+            <Link to={'/game?difficulty=hard'}>
+                <button className='home-buttons'>Hard(no clue)</button>
+            </Link>
+        </>
     )
 }
 
