da8562119a6edfe60f72b740e78a21e0bc90aa5e
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-14 16:13:04 +0530

    made so that it will work

--- quizz-app/src/components/QuizTable.tsx
+++ quizz-app/src/components/QuizTable.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
-const QuizTable = () => {
+import { Link } from 'react-router-dom'
+import { quizType } from '../data/userData'
+type QuizPropsType = {
+    from: string,
+    data: quizType[]
+}
+const QuizTable = (props: QuizPropsType) => {
+    const generateQuizData = props.data.map((quiz, index) => (
+        <article key={quiz.quiz_id} className='quiz-article'>
+            <h3>{quiz.quiz_name}</h3>
+            {props.from !== 'completed' ? <Link to={'/quiz/'}><button>Re-attempt Quiz</button></Link> : null}
+            <Link to={'/review/'}><button>Review Quiz</button></Link>
+            <Link to={'/ranking/'}><button>Rank</button></Link>
+        </article>
+    ))
     return (
-        <div>
-
-        </div>
+        <>
+            {generateQuizData}
+        </>
     )
 }
 
--- quizz-app/src/pages/CompletedQuiz.tsx
+++ quizz-app/src/pages/CompletedQuiz.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { homeDataType } from '../data/userData'
+import QuizTable from '../components/QuizTable'
 
 const CompletedQuiz = (props: { homeData: homeDataType }) => {
+    const cqdata = props.homeData.fully_attempted_quizzes
     return (
         <div>
-            CompletedQuiz
+            <QuizTable from='completed' data={cqdata} />
         </div>
     )
 }
