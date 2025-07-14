4601ee9809cb85308b8ee0e8f5b7a0e4551ac482
RohanVarmaP <rohanvarmapokala@gmail.com>
2025-07-14 14:31:53 +0530

    completed signup and log in

--- quizz-app/src/pages/Login.tsx
+++ quizz-app/src/pages/Login.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
-        <div>
-            Login
-        </div>
+        <form className='auth-section'>
+            <h4>Login</h4>
+            <label htmlFor='username'>
+                Username:
+                <input name='username' id='username' type="text" placeholder='user1' />
+            </label>
+            <label htmlFor='password'>
+                Password:
+                <input name='password' id='password' type="password" placeholder='password' />
+            </label>
+            <button type='submit'>login</button>
+            <p>don't have an account? <Link to={'/signup'}>Signup</Link></p>
+        </form>
     )
 }
 
--- quizz-app/src/pages/Signup.tsx
+++ quizz-app/src/pages/Signup.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
     return (
-        <div>
-            Signup
-        </div>
+        <form className='auth-section'>
+            <h4>Signup</h4>
+            <label htmlFor='username'>
+                Username:
+                <input name='username' id='username' type="text" placeholder='user1' />
+            </label>
+            <label htmlFor='password'>
+                Password:
+                <input name='password' id='password' type="password" placeholder='password' />
+            </label>
+            <label htmlFor='confirm_password'>
+                Confirm Password:
+                <input name='confirm_password' id='confirm_password' type="password" placeholder='re-enter password' />
+            </label>
+            <button type='submit'>login</button>
+            <p>don't have an account? <Link to={'/signup'}>Signup</Link></p>
+        </form>
     )
 }
 
